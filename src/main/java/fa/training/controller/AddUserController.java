6af2e52bc43de86aae2480d580cb8bd18316a1df
Noahvnp <norahpvn@gmail.com>
2025-02-07 15:32:32 +0700

    implement add new user security file

--- src/main/java/fa/training/controller/AddUserController.java
+++ src/main/java/fa/training/controller/AddUserController.java
@@ -8,14 +8,12 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+@CrossOrigin
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AddUserController {
     AddUserService userAddService;
@@ -23,7 +21,16 @@ public class AddUserController {
     @PostMapping
     public ApiResponse<UserResponseDTO> addUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
         return ApiResponse.<UserResponseDTO>builder()
+                .message("User " + userRequestDTO.getUserId() + " has been added...")
                 .result(userAddService.addUser(userRequestDTO))
+                .build();
+    }
+
+    @PostMapping("/user-sec-file")
+    public ApiResponse<UserResponseDTO> addUserSecFile(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+        return ApiResponse.<UserResponseDTO>builder()
+                .message("User " + userRequestDTO.getUserId() + " has been added...")
+                .result(userAddService.addUserSecFile(userRequestDTO))
                 .build();
     }
 }
