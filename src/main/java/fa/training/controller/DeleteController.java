53d09daa538cfd2aaea9eb1ff379c623066cf8ea
A18156 <laughingnguyen15@gmail.com>
2025-02-07 10:39:11 +0700

    hidden user after delete

--- src/main/java/fa/training/controller/DeleteController.java
+++ src/main/java/fa/training/controller/DeleteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/NV1")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@CrossOrigin
 public class DeleteController {
     DeleteUserService deleteUserService;
 
